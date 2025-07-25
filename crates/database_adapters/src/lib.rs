e0cf52e9181f75ee823e8de063322ea8698c78cd
lthorpe <lewis@UK-STLAPTOP-xBf.SideTrade.com>
2025-07-25 19:28:15 +0100

    commit

--- crates/database_adapters/src/lib.rs
+++ crates/database_adapters/src/lib.rs
@@ -1,26 +1,59 @@
 pub mod postgres;
 
+use std::fmt::Display;
 use logging::timeit;
 use tracing::info;
 use common::utils::read_sql_file;
 use dag::IntoDagNodes;
 
-pub trait DatabaseExecutor {
-    fn execute(&mut self, sql: &str) -> Result<(), ExecutorError>;
+pub enum DatabaseAdapterError {
+    InvalidConnectionError(String),
+    SyntaxError(String),
+    UnexpectedError(String),
+    IoError(std::io::Error),
+}
+
+impl Display for DatabaseAdapterError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DatabaseAdapterError::InvalidConnectionError(err) => {
+                write!(f, "Invalid connection details: {}", err)
+            }
+            DatabaseAdapterError::SyntaxError(err) => {
+                write!(f, "Syntax error: {}", err)
+            }
+            DatabaseAdapterError::UnexpectedError(err) => {
+                write!(f, "Unexpected error: {}", err)
+            }
+            DatabaseAdapterError::IoError(err) => {
+                write!(f, "I/O error: {}", err)
+            }
+        }
+    }
+}
+
+impl From<std::io::Error> for DatabaseAdapterError {
+    fn from(err: std::io::Error) -> Self {
+        DatabaseAdapterError::IoError(err)
+    }
+}
+
+pub trait DatabaseAdapter {
+    fn execute(&mut self, sql: &str) -> Result<(), DatabaseAdapterError>;
+    fn connection(&self) -> String;
     fn execute_dag_models<'a, T>(
         &mut self,
         nodes: T,
         compile_path: &str,
         models_dir: &str,
-    ) -> Result<(), ExecutorError>
+    ) -> Result<(), DatabaseAdapterError>
     where
         T: IntoDagNodes<'a>,
     {
         timeit!("Executed all models", {
             for node in nodes.into_vec() {
                 timeit!(format!("Executed model {}", &node.path.display()), {
-                    let sql = read_sql_file(models_dir, &node.path, compile_path)
-                        .map_err(|e| ExecutorError::InvalidFilePath(e.to_string()))?;
+                    let sql = read_sql_file(models_dir, &node.path, compile_path)?;
                     self.execute(&sql)?
                 });
             }
@@ -29,4 +62,3 @@ pub trait DatabaseExecutor {
         Ok(())
     }
 }
-
--- crates/database_adapters/src/postgres.rs
+++ crates/database_adapters/src/postgres.rs
@@ -1,9 +1,51 @@
-use postgres::Client;
-use crate::database::DatabaseExecutor;
-use crate::ExecutorError;
+use std::io::ErrorKind;
+use std::ops::{Deref, DerefMut};
+use postgres::{Client, Error, NoTls}
+;
+use postgres::error::SqlState;
+use crate::{DatabaseAdapter, DatabaseAdapterError};
 
-impl DatabaseExecutor for Client {
-    fn execute(&mut self, sql: &str) -> Result<(), ExecutorError> {
-        Ok(self.batch_execute(sql)?)
+impl From<Error> for DatabaseAdapterError {
+    fn from(err: Error) -> Self {
+        if let Some(e) = err.as_db_error() {
+            match e.code() {
+                &SqlState::CONNECTION_DOES_NOT_EXIST => {
+                    DatabaseAdapterError::InvalidConnectionError(e.to_string())
+                }
+                &SqlState::SYNTAX_ERROR => {
+                    DatabaseAdapterError::SyntaxError(e.to_string())
+                }
+                &SqlState::IO_ERROR => {
+                    DatabaseAdapterError::IoError(std::io::Error::new(ErrorKind::Other, e.to_string()))
+                }
+                _ => {
+                    DatabaseAdapterError::UnexpectedError(e.to_string())
+                }
+            }
+        } else {
+            DatabaseAdapterError::UnexpectedError(err.to_string())
+        }
     }
+}
+
+pub struct PostgresAdapter {
+    host: String,
+    port: u16,
+    db: String,
+    password: String,
+    user: String,
+}
+
+impl DatabaseAdapter for PostgresAdapter {
+    fn execute(&mut self, sql: &str) -> Result<(), DatabaseAdapterError> {
+        let mut conn = Client::connect(&self.connection(), NoTls)?;
+        let resp = conn.query(sql, &[])?;
+
+        Ok(())
+    }
+    fn connection(&self) -> String {
+        format!("host={} user={} password={} dbname={}",
+        self.host, self.user, self.password, self.db)
+    }
+
 }
